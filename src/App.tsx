3d4efb8a6df844af08fcd9a2a910b2034c0ead8d
cjhih456 <42570411+cjhih456@users.noreply.github.com>
2023-12-02 14:58:21 +0900

    define default head

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,18 @@ import { defineComponent, inject } from "vue";
 import { RouterView } from "vue-router";
 import { HeaderKey, provideHeader } from '@/plugins/LayoutHeaderPlugin'
 import DefaultLayout from "./layout/DefaultLayout";
+import { useHead } from "@unhead/vue";
+
 
 export default defineComponent({
   setup() {
+    useHead({
+      title: 'Vite vue unhead ssr ts starter',
+      meta: [
+        {"http-equiv": 'x-ua-compatible'},
+        {charset: 'utf-8'},
+      ]
+    })
     provideHeader()
     return () => <div>
       <DefaultLayout>
